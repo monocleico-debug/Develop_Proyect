38247a9722ea63de7d6534da99a9a87b1afc2dc0
monocleico-debug <pabuenamariana@gmail.com>
2026-04-30 13:59:21 -0500

    Creacion del Menu y clase Gas

--- src/Gas.java
+++ src/Gas.java
@@ -1,2 +1,22 @@
 public class Gas {
+
+    private double realGas; // m3
+    private int cookingFrequency; // baja, media, alta
+    private int kitchenType; // 1, 2, 3
+    private int usageTime; // 1h, 1-2h, +2h
+
+    public Gas(double realGas, int cookingFrequency, int kitchenType, int usageTime) {
+        this.realGas = realGas;
+        this.cookingFrequency = cookingFrequency;
+        this.kitchenType = kitchenType;
+        this.usageTime = usageTime;
+    }
+
+    public double getRealGas() {
+        return realGas;
+    }
+
+    public int getPoints() {
+        return cookingFrequency + kitchenType + usageTime; //puntos totales
+    }
 }
